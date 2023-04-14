540e5828a4f793cea5f8eb687f767d91c8f3b944
DeCorrean Davis <112592380+DeCorreanD@users.noreply.github.com>
2023-04-13 22:57:27 -0500

    Made all the changes to get code running smooth

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
-from appliances.kitchen import Dishwasher
-from appliances.laundry import Dryer
-from appliances.laundry import Washer
-from appliances.kitchen.utility import Refrigerator
+from appliances.kitchen import DishWasher, Refrigerator
+from appliances.laundry.dryer import Dryer
+from appliances.laundry.washer import Washer
+from appliances.kitchen import CoffeeMaker
+from appliances.kitchen import CanOpener
 
-whirlpool_dishwasher = Dishwasher("black")
+whirlpool_dishwasher = DishWasher("black")
 whirlpool_dishwasher.wash_dishes()
 
 samsung_washer = Washer("red")
@@ -14,3 +15,7 @@ lg_fridge.make_ice()
 
 mr_coffee = CoffeeMaker("white")
 mr_coffee.make_coffee()
+
+tuna_can = CanOpener("Gold")
+tuna_can.open_can()
+ 
